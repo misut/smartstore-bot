986cdcb5aad04683aee9ecd4c5d1f30515376f65
misut <pkr5207@gmail.com>
2022-04-04 00:32:51 +0900

    test: add check test

--- tests/infrastructure/test_chrome_errander.py
+++ tests/infrastructure/test_chrome_errander.py
@@ -1,6 +1,12 @@
 from bot.infrastructure import ChromeSmartStoreErrander
 
 
+def test_check_product(errander: ChromeSmartStoreErrander) -> None:
+    with errander:
+        assert not errander.check_product("linefriends", 4671768002)
+        assert errander.check_product("_minix", 5397361753)
+
+
 def test_fetch_product(errander: ChromeSmartStoreErrander) -> None:
     expected_name = "라인프렌즈 코니 베이비 앉은 인형 (35cm)"
     expected_price = 23000
