2d7e0b65ee7736d4df88c2edc1155e2cf00f178f
misut <pkr5207@gmail.com>
2022-04-05 23:43:52 +0900

    test: fix error while checking and fetching

--- tests/infrastructure/conftest.py
+++ tests/infrastructure/conftest.py
@@ -2,6 +2,7 @@ import os
 
 import dotenv
 import pytest
+import selenium
 
 from bot.infrastructure import ChromeSmartStoreErrander
 
@@ -10,7 +11,13 @@ from bot.infrastructure import ChromeSmartStoreErrander
 def initialize_chrome_errander() -> ChromeSmartStoreErrander:
     dotenv.load_dotenv()
 
+    option = selenium.webdriver.ChromeOptions()
+    option.add_argument("headless")
+    option.add_argument("window-size=1920x1080")
+    option.add_argument("disable-gpu")
+
     return ChromeSmartStoreErrander(
         username=os.getenv("SMARTSTORE_USERNAME"),
         password=os.getenv("SMARTSTORE_PASSWORD"),
+        option=option,
     )
--- tests/infrastructure/test_chrome_errander.py
+++ tests/infrastructure/test_chrome_errander.py
@@ -1,12 +1,13 @@
 from loguru import logger
 
+from bot.domain import StoreType
 from bot.infrastructure import ChromeSmartStoreErrander
 
 
 def test_check_product(errander: ChromeSmartStoreErrander) -> None:
     with errander:
-        assert not errander.check_product("linefriends", 4671768002)
-        assert errander.check_product("_minix", 5397361753)
+        assert not errander.check_product(6490282859, "samlip", StoreType.BRAND)
+        assert errander.check_product(5397361753, "_minix")
 
 
 def test_fetch_product(errander: ChromeSmartStoreErrander) -> None:
@@ -14,7 +15,7 @@ def test_fetch_product(errander: ChromeSmartStoreErrander) -> None:
     expected_price = 23000
 
     with errander:
-        product = errander.fetch_product("linefriends", 4671768002)
+        product = errander.fetch_product(4671768002, "linefriends")
 
     assert product.name == expected_name
     assert product.price == expected_price
@@ -23,7 +24,7 @@ def test_fetch_product(errander: ChromeSmartStoreErrander) -> None:
 
 def test_fetch_product_with_option_price(errander: ChromeSmartStoreErrander) -> None:
     with errander:
-        product = errander.fetch_product("cjang", 2166508785)
+        product = errander.fetch_product(2166508785, "cjang")
     
     logger.info(f"{product.options_list[1].options[0].name}: {product.options_list[1].options[0].price}")
     assert len(product.options_list) == 6
